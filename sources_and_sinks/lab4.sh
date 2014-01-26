6c3a8baa9a642ddf31133dfa4e1b075d2f6520a2
Aristoteles Panaras <spiritual.dragon.of.ra@gmail.com>
2014-01-26 13:24:00 +0200

    Updated lab4 script

--- sources_and_sinks/lab4.sh
+++ sources_and_sinks/lab4.sh
@@ -6,12 +6,13 @@ outfile="lab4_output.text"
 #question_template: "command | tee -a $outfile"
 questions=(
 #Q_4_a
-	"tshark -Y "{'"icmp.type == 8" -n','"icmp.type == 8" -N "nNC"','"ip.dst == ulysses.noc.ntua.gr"','"ip.src == ulysses.noc.ntua.gr" -T fields -e ip.id','"ip.src == www.uoa.gr" -PV','"ip.src == www.auth.gr || ip.dst == www.auth.gr"'}" -r \"$INFILE\" | tee -a $outfile"
+#	"tshark -Y "{'"icmp.type == 8" -n','"icmp.type == 8" -N "nNC"','"ip.dst == ulysses.noc.ntua.gr"','"ip.src == ulysses.noc.ntua.gr" -T fields -e ip.id','"ip.src == www.uoa.gr" -PV','"ip.src == www.auth.gr || ip.dst == www.auth.gr"'}" -r \"$INFILE\" | tee -a $outfile"
 #Q_4_b
-#"touch ~/index.txt"
-#	"echo '01' > ~/serial"
-#	"openssl "{'genrsa -out gened.key','req -new -key gened.key -keyform PEM -out requed.csr','ca -in requed.csr -out fin.crt','s_client -state -host sorch.netmode.ntua.gr -port 443','s_client -state -host sorch.netmode.ntua.gr -port 443 -cert fin.crt -key gened.key'}" 2>&1 | tee -a $outfile"
-	####On openssl queries we have to type answers, and on the last we have to type "GET /netmg.php HTTP/1.0\n\n", since the server has no expect shell.
+	"touch ~/index.txt"
+	"echo '01' > ~/serial"
+	"openssl "{'genrsa -out gened.key','req -subj "/C=GR/ST=ATTIKI/L=ATHENS/O=MY_MYSELF_I/CN=my.domain.netmode.ntua.gr" -new -key gened.key -keyform PEM -out requed.csr','ca -in requed.csr -out fin.crt','s_client -state -host sorch.netmode.ntua.gr -port 443','s_client -state -host sorch.netmode.ntua.gr -port 443 -cert fin.crt -key gened.key'}" 2>&1 | tee -a $outfile"
+#	"openssl s_client -state -host sorch.netmode.ntua.gr -port 443 -cert fin.crt -key gened.key < lab4_ssl_command.txt 2>&1 | tee -a $outfile"
+	####On the last we have to type "GET /netmg.php HTTP/1.0\n\n", since the server has no expect shell.
 )
 
 function report () {
